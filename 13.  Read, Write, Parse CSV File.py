77eff654996a7a4b8d582bc7e6931e31b59d8cc8
pk.airsqn@gmail.com <India%17>
2019-12-17 18:22:20 +0530

    Regular Expressions

---
+++ 13.  Read, Write, Parse CSV File.py
@@ -0,0 +1,23 @@
+import  csv
+with open('sample.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    # print(csv_reader)
+
+''' Using dictionary Reader'''
+with open('sample.csv', 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+
+    for line in csv_reader:
+        print(line['policyID'])
+
+    '''to read the file
+    for line in csv_reader:
+        print(line)
+    '''
+    # next(csv_reader)
+
+    # with open('new_sample.csv','w') as ns:
+    #     csv_writer = csv.writer(ns, delimiter='\t')
+    #     for line in csv_reader:
+    #         csv_writer.writerow(line)
+    #
