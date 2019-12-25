8c4a525dd06fe9490d87fde7488cc5fb6a4cff2a
pk.airsqn@gmail.com <India%17>
2019-12-26 02:29:16 +0530

    automate file opening

---
+++ Context Managers - managing resources.py
@@ -0,0 +1,67 @@
+# '''we are gonna create a script which will open the file and close it auto'''
+# class Open_File():
+#
+#     #going to accept arguments lso access attri that we set from our other methods
+#     def __init__(self, filename, mode):
+#         self.filename = filename
+#         self.mode = mode
+#
+#     def __enter__(self):
+#         self.file = open(self.filename, self.mode)
+#         return self.file
+#
+#     def __exit__(self, exc_type, exc_val, traceback):
+#         self.file.close()
+#
+# with Open_File('sampl.txt', 'w') as f:
+#     f.write('Testing...')
+#
+# print(f.closed)
+
+
+# # USING FUNCTIONS
+# from contextlib import contextmanager
+#
+# @contextmanager
+# def open_file(file, mode):
+#     try:
+#         f = open_file(file, mode)
+#         yield f
+#     finally:
+#         f.close()
+#
+# with open_file('abc.txt', 'w') as f:
+#     f.write('Testing using functions!!')
+#
+# print(f.closed)
+
+
+#using OS module
+import os
+from contextlib import  contextmanager
+
+# cwd = os.getcwd()
+# os.chdir('Sample-Dir_One')
+# print(os.listdir())
+# os.chdir()
+#
+# cwd = os.getcwd()
+# os.chdir('Sample-Dir_Two')
+# print(os.listdir())
+# os.chdir()
+
+@contextmanager
+def change_dir(destnation):
+    try:
+        cwd = os.getcwd()
+        os.chdir(destnation)
+        yield
+
+    finally:
+        os.chdir(cwd)
+
+with change_dir('pract'):
+    print(os.listdir())
+
+with change_dir('PycharmProjects'):
+    print(os.listdir())
--- How many days to complete a task.py
+++ How many days to complete a task.py
@@ -1,29 +1,31 @@
 import datetime
 import math
 
-current_weight =220
-goal_weight =180
-avg_lbs_week = 2
-
-start_date = datetime.date.today()
-end_date = start_date
-
-while current_weight > goal_weight:
-    end_date += datetime.timedelta(days=7)
-    current_weight -= avg_lbs_week
-
-print(end_date)
-#pirnt no of weeks to complete the task
-print(f'reached goal in {(end_date - start_date).days // 7} weeks')
-
-
-# goal_subs = 150000
-# current_subs = 85000
-# subs_to_go = goal_subs - current_subs
+# current_weight =220
+# goal_weight =180
+# avg_lbs_week = 2
 #
-# avg_subs_day = 200
-# days_to_go = math.ceil(subs_to_go / avg_subs_day)
+# start_date = datetime.date.today()
+# end_date = start_date
 #
-# today = datetime.date.today()
+# while current_weight > goal_weight:
+#     end_date += datetime.timedelta(days=7)
+#     current_weight -= avg_lbs_week
 #
-# print(today + datetime.timedelta(days=days_to_go))
+# print(end_date)
+# #pirnt no of weeks to complete the task
+# print(f'reached goal in {(end_date - start_date).days // 7} weeks')
+#
+
+
+'''to reach a goal'''
+goal_subs = 150000
+current_subs = 85000
+subs_to_go = goal_subs - current_subs
+
+avg_subs_day = 200
+days_to_go = math.ceil(subs_to_go / avg_subs_day)   #ciel will round up the value
+
+today = datetime.date.today()
+
+print('No of days to go : ', today + datetime.timedelta(days=days_to_go))
