a09a355135b682f7fc585826a557f2d392642435
pk.airsqn@gmail.com <India%17>
2019-12-27 06:17:18 +0530

    password hiding

---
+++ Programs for PRACTICE/Hinding Passwords & Secret Jeys ub Env.Variables.py
@@ -0,0 +1,9 @@
+import os
+# db_user = os.environ.get('DB_USER')
+# db_password = os.environ.get('DB_PASS')
+
+db_user = 'my_db_user'
+db_password = 'my_db_pass_123!'
+
+print(db_user)
+print(db_password)
