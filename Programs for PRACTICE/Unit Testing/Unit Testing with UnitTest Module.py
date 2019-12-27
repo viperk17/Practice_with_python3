3ef9da93b11f7446b741385c3d06d78353ce6c9b
pk.airsqn@gmail.com <India%17>
2019-12-27 07:30:30 +0530

    unit testing

---
+++ Programs for PRACTICE/Unit Testing/Unit Testing with UnitTest Module.py
@@ -0,0 +1,24 @@
+
+def add(x, y):
+    """Add Function"""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract Function"""
+    return x - y
+
+
+def multiply(x, y):
+    """Multiply Function"""
+    return x * y
+
+
+def divide(x, y):
+    """Divide Function"""
+    # return x / y
+    if y==0:
+        raise ValueError('Cannot divide by zero')
+    return x/y
+
+# print(add(15,25))
