823f63c5d5fd12c9de2056c0034e2d01327bb764
pk.airsqn@gmail.com <India%17>
2019-12-29 23:59:59 +0530

    OOPS

---
+++ OOPS Concepts/OOPS 9 - Public | Private | Protected Access Specifiers.py
@@ -0,0 +1,34 @@
+class Employee:
+    var = 8
+    _protec = 9
+    __private = 90  #nameangling
+    no_of_leaves = 8
+
+    def __init__(self, name, asalary, arole):
+        self.name = name
+        self.salary = asalary
+        self.role = arole
+
+    # method
+    def print_details(self):
+        return f"Name is {self.name}. Salary is {self.salary}. ROle is {self.role}"
+
+    @classmethod
+    def change_leaves(cls, newleaves):
+        cls.no_of_leaves = newleaves
+
+    @classmethod
+    def from_str(cls, string):
+        # params = string.split("-")
+        # print(params)
+        # return cls(params[0], params[1], params[2])
+        return cls(*string.split("-"))
+
+    @staticmethod
+    def print_good(string):
+        print("This is good " + string)
+
+
+emp = Employee("harry", 454, "Programmer")
+print(emp._protec)
+print(emp._Employee__private)
