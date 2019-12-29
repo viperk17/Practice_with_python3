54dabd4b42736c620e02bb170b5cd554e7a3f04d
pk.airsqn@gmail.com <India%17>
2019-12-29 22:02:29 +0530

    OOPS

---
+++ OOPS Concepts/OOPS 1.py
@@ -0,0 +1,28 @@
+class Employee:
+    no_of_leaves = 8
+
+    def __init__(self, name, asalary, arole):
+        self.name = name
+        self.salary = asalary
+        self.role = arole
+
+    #method
+    def print_details(self):
+        return f"Name is {self.name}. Salary is {self.salary}. ROle is {self.role}"
+
+harry = Employee()
+rohan = Employee()
+
+# harry.name = "Harry"
+# harry.salary = 455
+# harry.role = "Instructor"
+
+# rohan.name = "Rohan"
+# rohan.salary = 5664
+# rohan.role = "Student"
+
+# print(Employee.no_of_leaves)
+# print(Employee.__dict__)
+# Employee.no_of_leaves = 9
+
+print(Employee.no_of_leaves)
