c989e2a5357b16614d1c194c95bdaa2e982a8e82
Prashant Singh <pk.airsqn@protonmail.com>
2021-04-13 15:04:01 +0530

    apr update

--- Programs for PRACTICE/Enumerate Function/Enumerate Function.py
+++ Programs for PRACTICE/Enumerate Function/Enumerate Function.py
@@ -1,13 +1,13 @@
-l1 = ['Bhindi', 'Aloo','Noodles','chopsticks']
+l1 = ['Bhindi', 'Aloo', 'Noodles', 'chopsticks']
 
 # i=1
 # for item in l1:
 #     if i%2 is not 0:
-#         print(f"Please buy (item)")
+#         print(f"Please buy {item}")
 #     i+=1
 
-#using enum
+# using enum
 for index, item in enumerate(l1):
-    #for index starting at 1 as by default it starts at zero
-    if index % 2 ==0:
+    # for index starting at 1 as by default it starts at zero
+    if index % 2 == 0:
         print(f"Please buy {item}")
